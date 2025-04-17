7d47e835de7e9a11e5203174141097e2d89bd791
lizziesalinas <lizardcat2002@gmail.com>
2025-04-16 18:13:14 -0700

    Update sketch.js

--- personal_site/js/sketch.js
+++ personal_site/js/sketch.js
@@ -1,14 +1,16 @@
 let canvas;
 let bugImg;
 
-function preload() {
-    bugImg = loadImage('images/bug.png');
-}
+// function preload() {
+//     bugImg = loadImage('images/bug.png');
+// }
 
 function setup() {
     canvas = createCanvas(windowWidth, windowHeight);
     canvas.position(0, 0);
     canvas.style("z-index", "-1");
+
+    bugImg = loadImage('images/bug.png');
 }
 
 function windowResized() {
@@ -16,8 +18,12 @@ function windowResized() {
 }
 
 function draw() {
+    background(255);
+    if (bugImg) {
+        image(bugImg, mouseX, mouseY, 20, 20);
+    }
 }
 
-function mouseMoved() {
-    image(bugImg, mouseX, mouseY, 40, 40);
-}
+// function mouseMoved() {
+//     image(bugImg, mouseX, mouseY, 40, 40);
+// }
