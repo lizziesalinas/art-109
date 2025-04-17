e65d6010277eb20a311a604de7966c3e2dc438bd
lizziesalinas <lizardcat2002@gmail.com>
2025-04-16 17:54:35 -0700

    bug

--- personal_site/js/sketch.js
+++ personal_site/js/sketch.js
@@ -1,10 +1,14 @@
 let canvas;
+let bugImg;
+
+function preload() {
+    bugImg = loadImage('images/bug.png'); // Store image in a variable
+}
 
 function setup() {
     canvas = createCanvas(windowWidth, windowHeight);
     canvas.position(0, 0);
     canvas.style("z-index", "-1");
-    //background(255);
 }
 
 function windowResized() {
@@ -12,9 +16,12 @@ function windowResized() {
 }
 
 function draw() {
+    // Optionally: background(255); // Uncomment if you want a white canvas
 }
 
 function mouseMoved() {
     fill(255, 30, 100);
-    ellipse(mouseX, mouseY, 20, 20);
+    noStroke(); // Optional, removes border around fill
+    ellipse(mouseX, mouseY, 10, 10); // Show the fill color effect
+    image(bugImg, mouseX, mouseY, 20, 20); // Use loaded image variable
 }
